4af052c8137d079200c18f72a487e438fadf5385
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-12 02:35:18 +0530

    [Rupesh Patil].Updated Book Controller

--- BookStore/src/main/java/com/bookstore/controller/BookController.java
+++ BookStore/src/main/java/com/bookstore/controller/BookController.java
@@ -3,28 +3,44 @@ package com.bookstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookstore.model.Book;
+import com.bookstore.response.BookResponse;
 import com.bookstore.service.IBookService;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
-@RequestMapping("/books")
-@Slf4j
 @CrossOrigin
+@RequestMapping("/books")
 public class BookController {
 	
 	@Autowired
 	IBookService bookservice;
+	
 	@PostMapping("/add")
-	public ResponseEntity<String> addBook(@RequestBody Book book){
-		log.info("controller");
+	public ResponseEntity<BookResponse> addBook(@RequestBody Book book){
 		return bookservice.addBook(book);
-		
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<BookResponse> updateBook(@RequestBody Book book,@RequestParam("bookname") String bookName){
+		return bookservice.updateBookDetails(bookName, book);
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<BookResponse> removeBook(@RequestParam("bookname") String bookName){
+		return bookservice.removeBook(bookName);
+	}
+	
+	@GetMapping("/getAllBooks")
+	public ResponseEntity<BookResponse> getBooks(){
+		return bookservice.getAllBooks();
 	}
 }

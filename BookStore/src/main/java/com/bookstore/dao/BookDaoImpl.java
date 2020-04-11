75367cadea33ca89694a542c51d2609d035591e5
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-12 02:35:40 +0530

    [Rupesh Patil].Updated BookDAO implementation

--- BookStore/src/main/java/com/bookstore/dao/BookDaoImpl.java
+++ BookStore/src/main/java/com/bookstore/dao/BookDaoImpl.java
@@ -1,11 +1,16 @@
 package com.bookstore.dao;
 
-import javax.persistence.NoResultException;
+import java.util.List;
 
+import javax.persistence.NoResultException;
+import javax.transaction.Transactional;
+
+import org.hibernate.StaleObjectStateException;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.bookstore.exception.InternalServerError;
 import com.bookstore.model.Book;
 import com.bookstore.util.HibernateUtil;
 
@@ -16,25 +21,67 @@ public class BookDaoImpl implements IBookDAO {
 	HibernateUtil<Book> hibernateUtil;
 
 	@Override
+	@Transactional
 	public void addBook(Book book) {
-		hibernateUtil.save(book);
+		try {
+			hibernateUtil.save(book);
+		} catch (Exception e) {
+			throw new InternalServerError();
+		}
 	}
 
 	@Override
+	@Transactional
 	public Book getCurrentBook(int bookCode) {
-		return hibernateUtil.getBook(bookCode);
+		try {
+			return hibernateUtil.getBook(bookCode);
+		} catch (Exception e) {
+			throw new InternalServerError();
+		}
 	}
 
 	@Override
+	@Transactional
 	public Book getBookByName(String bookName) {
 		try {
-		String query = "From Book where bookName=:name";
+			String query = "From Book where bookName=:name";
 
-		Query<Book> book = hibernateUtil.select(query);
-		book.setParameter("name", bookName);
-		return book.getSingleResult();
-		}catch (NoResultException e) {
+			Query<Book> book = hibernateUtil.select(query);
+			book.setParameter("name", bookName);
+			return book.getSingleResult();
+		} catch (NoResultException e) {
 			return null;
+		}
+	}
+
+	@Override
+	@Transactional
+	public int deleteBook(String bookName) {
+		String query = "DELETE FROM Book where bookName=:name";
+		Query<Book> hQuery = hibernateUtil.createQuery(query);
+		hQuery.setParameter("name", bookName);
+		return hQuery.executeUpdate();
+	}
+
+	@Override
+	@Transactional
+	public void updateBook(Book book, String bookName) {
+		try {
+			hibernateUtil.update(book);;
+		} catch (StaleObjectStateException e) {
+			throw new InternalServerError();
+		}
+	}
+
+	@Override
+	@Transactional
+	public List<Book> getAllBooks() {
+		try {
+			String query = "FROM Book";
+			Query<Book> books = hibernateUtil.select(query);
+			return books.list();
+		} catch (Exception e) {
+			throw new InternalServerError();
 		}
 	}
 }

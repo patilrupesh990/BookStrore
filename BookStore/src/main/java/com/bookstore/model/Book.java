35225731d457fed9e8d6d2567a7e82ebaf7abfdf
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-12 00:40:02 +0530

    [Rupesh Patil].Modified Book Bean file

--- BookStore/src/main/java/com/bookstore/model/Book.java
+++ BookStore/src/main/java/com/bookstore/model/Book.java
@@ -1,5 +1,43 @@
 package com.bookstore.model;
 
-public class Book {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
+@Data
+@Table(name="books_details")
+@Entity
+public class Book {
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int bookId;
+	
+	@Column
+	@NotNull
+	private String bookName;
+	
+	@Column
+	private String bookCode;
+
+	@Column
+	@NotNull
+	private String bookDetails;
+	
+	@Column
+	@NotNull
+	private Double price;
+	
+	@Column
+	@NotNull
+	private int quantity;
+	
+	@Column
+	@NotNull
+	private String authorName;
 }

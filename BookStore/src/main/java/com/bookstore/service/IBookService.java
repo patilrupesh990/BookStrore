d047c1d28e91be3686e9ea9196fb6313c54115e7
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-12 02:34:19 +0530

    [Rupesh Patil].Updated BookService InterFace

--- BookStore/src/main/java/com/bookstore/service/IBookService.java
+++ BookStore/src/main/java/com/bookstore/service/IBookService.java
@@ -1,16 +1,17 @@
 package com.bookstore.service;
 
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 
 import com.bookstore.model.Book;
+import com.bookstore.response.BookResponse;
 
 public interface IBookService {
 	
-	public ResponseEntity<String> addBook(Book book);
+	public ResponseEntity<BookResponse> addBook(Book book);
 	
-	public RequestEntity<Object> removeBook();
+	public ResponseEntity<BookResponse> removeBook(String bookName);
 	
-	public RequestEntity<Object> getAllBooks();
+	public ResponseEntity<BookResponse> getAllBooks();
 	
+	public ResponseEntity<BookResponse> updateBookDetails(String bookName,Book updatedBook);
 }

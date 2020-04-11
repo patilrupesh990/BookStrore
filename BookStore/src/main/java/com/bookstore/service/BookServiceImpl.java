dca020789fe04f1c1684a8fa8db849737dde0f5a
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-12 02:34:38 +0530

    [Rupesh Patil].Updated BooService Implementation

--- BookStore/src/main/java/com/bookstore/service/BookServiceImpl.java
+++ BookStore/src/main/java/com/bookstore/service/BookServiceImpl.java
@@ -2,40 +2,67 @@ package com.bookstore.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.dao.IBookDAO;
+import com.bookstore.exception.BookAlreadyExist;
+import com.bookstore.exception.BookNotFoundException;
 import com.bookstore.model.Book;
-import com.bookstore.response.BookAlreadyExist;
+import com.bookstore.response.BookResponse;
 
 @Service
-public class BookServiceImpl implements IBookService{
+public class BookServiceImpl implements IBookService {
 
 	@Autowired
 	IBookDAO bookdao;
+
 	@Override
-	public ResponseEntity<String> addBook(Book book) {
-		String bookName=book.getBookName().toLowerCase().trim();
-		if(bookdao.getBookByName(bookName)==null) {
+	public ResponseEntity<BookResponse> addBook(Book book) {
+		String bookName = book.getBookName().toLowerCase().trim();
+		if (bookdao.getBookByName(bookName) == null) {
 			bookdao.addBook(book);
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new String("added book"));
-		}else {
+			return ResponseEntity.status(HttpStatus.ACCEPTED)
+					.body(new BookResponse(book, 202, book.getBookName() + " Book Added"));
+		} else {
 			throw new BookAlreadyExist();
 		}
 	}
 
 	@Override
-	public RequestEntity<Object> removeBook() {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<BookResponse> removeBook(String bookName) {
+		if (bookdao.getBookByName(bookName) != null) {
+			if (bookdao.deleteBook(bookName) > 0)
+				return ResponseEntity.status(HttpStatus.ACCEPTED)
+						.body(new BookResponse(202, bookName + " Deleted Successfully"));
+			else
+				return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+						.body(new BookResponse(502, bookName + " Could Not delete Please Try Again"));
+		} else {
+			throw new BookNotFoundException();
+		}
+
 	}
 
 	@Override
-	public RequestEntity<Object> getAllBooks() {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<BookResponse> getAllBooks() {
+		if (bookdao.getAllBooks() != null) {
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(
+					new BookResponse(202, "Total Books Are:" + bookdao.getAllBooks().size(), bookdao.getAllBooks()));
+		} else {
+			throw new BookNotFoundException();
+		}
 	}
-	
+
+	@Override
+	public ResponseEntity<BookResponse> updateBookDetails(String bookName, Book updatedBook) {
+		if (bookdao.getBookByName(updatedBook.getBookName()) != null) {
+			bookdao.updateBook(updatedBook, bookName);
+			return ResponseEntity.status(HttpStatus.ACCEPTED)
+					.body(new BookResponse(updatedBook, 202, bookName + " Updated"));
+		} else {
+			throw new BookNotFoundException();
+		}
+	}
+
 }

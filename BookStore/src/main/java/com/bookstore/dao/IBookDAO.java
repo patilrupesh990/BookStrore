efa537fca83271c680b84cf273443755cc50c83f
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-12 02:35:56 +0530

    [Rupesh Patil].Updated BookDAO implementation

--- BookStore/src/main/java/com/bookstore/dao/IBookDAO.java
+++ BookStore/src/main/java/com/bookstore/dao/IBookDAO.java
@@ -1,9 +1,15 @@
 package com.bookstore.dao;
 
+import java.util.List;
+
 import com.bookstore.model.Book;
 
 public interface IBookDAO {
 	public void addBook(Book book);
+	public void updateBook(Book book,String bookName);
 	public Book getCurrentBook(int bookName);
 	public Book getBookByName(String bookName);
+	public int deleteBook(String bookName);
+	public List<Book> getAllBooks();
+	
 }

131b7b07a6e2ceac355b2e038fa0a14a77655c37
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-13 11:08:39 +0530

    [Rupesh Patil].Updated UserDAO interface

---
+++ BookStore-UserApi/src/main/java/com/bookstore/user/dao/IUserDAO.java
@@ -0,0 +1,12 @@
+package com.bookstore.user.dao;
+
+import com.bookstore.user.model.User;
+
+public interface IUserDAO {
+
+	public int register(User user);
+	public User isUserExist(User user);
+	public int activateUSer(int id);
+	public User getUser(String userName);
+	public User loginUser(String email,String password);
+}

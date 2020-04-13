d642ad15dfe64f09f8ec61e51beefd8232b72576
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-14 00:34:57 +0530

    [Rupesh Patil].Updated BookStoreApllication Main Class
    With RestTemplate Bean Method

--- BookStore/src/main/java/com/bookstore/BookStoreApplication.java
+++ BookStore/src/main/java/com/bookstore/BookStoreApplication.java
@@ -2,12 +2,20 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }

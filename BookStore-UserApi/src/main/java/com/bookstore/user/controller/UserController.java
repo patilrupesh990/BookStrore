6ea1c1a7682224e0f7066aae52a8b1ce1a2f5369
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-17 15:12:37 +0530

    [Rupesh Patil].updated UserController added SellerLogin & registration Api

--- BookStore-UserApi/src/main/java/com/bookstore/user/controller/UserController.java
+++ BookStore-UserApi/src/main/java/com/bookstore/user/controller/UserController.java
@@ -1,5 +1,4 @@
 package com.bookstore.user.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,17 +16,25 @@ import com.bookstore.user.response.UserResponse;
 import com.bookstore.user.service.IUserService;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	IUserService service;
-	
+
 	@PostMapping("/register")
 	public ResponseEntity<UserResponse> registerUser(@RequestBody User user){
+		user.setSeller(false);
 		return service.register(user);
 	}
+	
+	@PostMapping("/seller-register")
+	public ResponseEntity<UserResponse> sellerRegister(@RequestBody User user){
+		user.setSeller(true);
+		return service.register(user);
+	}
+	
 	
 	@GetMapping("/activ/{token}")
 	public ResponseEntity<UserResponse> activateUserAccount(@PathVariable("token")String token){
@@ -35,13 +42,18 @@ public class UserController {
 	}
 	
 	@PostMapping("/login/{email}")
-	public ResponseEntity<UserResponse> loginUser(@PathVariable("email") String email,@RequestHeader("password") String password) {
+	public ResponseEntity<Object> loginUser(@PathVariable("email") String email,@RequestHeader("password") String password) {
 		return service.loginUser(email,password);
 	}
+	
+	@PostMapping("/seller-login/{email}")
+	public ResponseEntity<Object> sellerLogin(@PathVariable("email") String email,@RequestHeader("password") String password) {
+		return service.loginAdmin(email,password);
+	}
+	
 	@GetMapping("/{token}")
 	public ResponseEntity<UserData> getUserById(@PathVariable("token") String token) {
-//		return service.getUserByID(token);
-		return null;
+		return service.getUserByID(token);
 	}
-
+	
 }

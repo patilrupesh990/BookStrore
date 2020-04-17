0f67731801d66565a378f17968c4c4cbd0891a16
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-17 15:09:33 +0530

    [Rupesh Patil].Updated UserService with registration and added sellrLogin

--- BookStore-UserApi/src/main/java/com/bookstore/user/service/UserServiceImpl.java
+++ BookStore-UserApi/src/main/java/com/bookstore/user/service/UserServiceImpl.java
@@ -2,11 +2,14 @@ package com.bookstore.user.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.bookstore.user.configuration.PasswordEncoderConfiguration;
 import com.bookstore.user.dao.IUserDAO;
@@ -14,6 +17,8 @@ import com.bookstore.user.exception.AuthenticationFailedException;
 import com.bookstore.user.exception.InvalidTokenOrExpiredException;
 import com.bookstore.user.exception.UserAlradyRegisterException;
 import com.bookstore.user.model.User;
+import com.bookstore.user.response.LoginSuccessResponse;
+import com.bookstore.user.response.UserData;
 import com.bookstore.user.response.UserResponse;
 import com.bookstore.user.util.DateValidator;
 import com.bookstore.user.util.EmailGenerator;
@@ -32,6 +37,8 @@ public class UserServiceImpl implements IUserService {
 	private EmailGenerator emailGenerate;
 	@Autowired
 	private JwtTokenUtil generateToken;
+	@Autowired
+	private UserData userresponce;
 
 	@Override
 	public ResponseEntity<UserResponse> register(User user) {
@@ -57,10 +64,10 @@ public class UserServiceImpl implements IUserService {
 
 	@Override
 	public ResponseEntity<UserResponse> activateUser(String token) {
-		int id=0;
+		int id = 0;
 		try {
 			id = generateToken.parseToken(token);
-		}catch (SignatureVerificationException e) {
+		} catch (SignatureVerificationException | AlgorithmMismatchException e) {
 			throw new InvalidTokenOrExpiredException("Invalid Token or Token Expired", HttpStatus.BAD_REQUEST);
 		}
 		if (userDao.activateUSer(id) > 0) {
@@ -71,15 +78,41 @@ public class UserServiceImpl implements IUserService {
 		}
 	}
 
-	public ResponseEntity<UserResponse> loginUser(String email, String password) {
+	@Override
+	public ResponseEntity<Object> loginUser(String email, String password) {
 		Optional<User> user = Optional.ofNullable(userDao.getUser(email));
 		if (user.isPresent() && passwordEncryption.passwordEncoder().matches(password, user.get().getPassword())) {
-			return ResponseEntity.status(HttpStatus.ACCEPTED)
-					.body(new UserResponse(202, "User Login Successfull WellCome Mr." + user.get().getFirstName() + " "
-							+ user.get().getLastName()));
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new LoginSuccessResponse(user.get().getFirstName(),
+					user.get().getLastName(), generateToken.generateToken(user.get().getUId()), "Login Success", 202));
 		} else {
 			throw new AuthenticationFailedException("Invalid UserName Or Password", HttpStatus.BAD_REQUEST);
 		}
 	}
 
+	public ResponseEntity<Object> loginAdmin(String email, String password) {
+		Optional<User> user = Optional.ofNullable(userDao.getUser(email));
+		if (user.isPresent() && user.get().isSeller()) {
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new LoginSuccessResponse(user.get().getFirstName(),
+					user.get().getLastName(), generateToken.generateToken(user.get().getUId()), "Login Success", 202));
+		} else {
+			throw new AuthenticationFailedException("Invalid UserName Or Password", HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@Override
+	public ResponseEntity<UserData> getUserByID(String token) {
+		try {
+			if (userDao.getUserById(generateToken.parseToken(token)) != null) {
+				User user = userDao.getUserById(generateToken.parseToken(token));
+				BeanUtils.copyProperties(user, userresponce);
+				userresponce.setStatus(302);
+				userresponce.setResponse("User Data Found");
+				return ResponseEntity.status(HttpStatus.FOUND).body(userresponce);
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new UserData(404, "User Not Found"));
+			}
+		} catch (SignatureVerificationException | JWTDecodeException | AlgorithmMismatchException e) {
+			throw new InvalidTokenOrExpiredException("Invalid Token or Token Expired", HttpStatus.BAD_REQUEST);
+		}
+	}
 }

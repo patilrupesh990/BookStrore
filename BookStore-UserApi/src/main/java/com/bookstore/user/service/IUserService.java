c083ac07465acd6c86c8b214fee79bbcc79cd7b4
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-17 15:11:10 +0530

    [Rupesh Patil].added Seller Login Functionality

--- BookStore-UserApi/src/main/java/com/bookstore/user/service/IUserService.java
+++ BookStore-UserApi/src/main/java/com/bookstore/user/service/IUserService.java
@@ -3,10 +3,13 @@ package com.bookstore.user.service;
 import org.springframework.http.ResponseEntity;
 
 import com.bookstore.user.model.User;
+import com.bookstore.user.response.UserData;
 import com.bookstore.user.response.UserResponse;
 
 public interface IUserService {
 	public ResponseEntity<UserResponse> register(User user);
 	public ResponseEntity<UserResponse> activateUser(String token);
-	public ResponseEntity<UserResponse> loginUser(String email, String password);
+	public ResponseEntity<Object> loginUser(String email, String password);
+	public ResponseEntity<UserData> getUserByID(String token);
+	public ResponseEntity<Object> loginAdmin(String email, String password);
 }

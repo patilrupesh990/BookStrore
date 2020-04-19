2a6400628e7b5bcabe7bd79f0a3934fc41fbd165
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-19 13:43:30 +0530

    [Rupesh Patil].updated Swagger Configuration

--- BookStore/src/main/java/com/bookstore/config/SwaggerConfig.java
+++ BookStore/src/main/java/com/bookstore/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.bookstore.controller")).paths(regex("/books.*"))
+				.apis(RequestHandlerSelectors.basePackage("com.bookstore.controller")).paths(regex("/.*"))
 				.build();
 	}
 }

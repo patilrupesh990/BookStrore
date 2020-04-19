da28c156ed3fb754c650f50d1705002a98052a3d
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-19 13:45:30 +0530

    [Rupesh Patil].Updated Model Book Entity

--- BookStore/src/main/java/com/bookstore/model/Book.java
+++ BookStore/src/main/java/com/bookstore/model/Book.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -46,4 +47,9 @@ public class Book {
 	@Column
 	@NotNull
 	private String authorName;
+	
+	@Lob
+	@Column(columnDefinition="BLOB")
+	private byte[] bookImage;
+	
 }

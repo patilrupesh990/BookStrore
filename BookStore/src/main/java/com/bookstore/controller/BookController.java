3ea1135ce29adbbb0f34e8c99a44aa0f0aaf294c
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-19 13:16:32 +0530

    [Rupesh Patil].updated BookController

--- BookStore/src/main/java/com/bookstore/controller/BookController.java
+++ BookStore/src/main/java/com/bookstore/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.bookstore.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,13 +14,14 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bookstore.model.Book;
 import com.bookstore.response.BookResponse;
 import com.bookstore.service.IBookService;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("*")
 @RequestMapping("/books")
 public class BookController {
 	
@@ -36,8 +39,13 @@ public class BookController {
 	}
 	
 	@DeleteMapping("/delete")
-	public ResponseEntity<BookResponse> removeBook(@RequestParam("bookname") String bookName,@RequestHeader String token){
-		return bookservice.removeBook(bookName,token);
+	public ResponseEntity<BookResponse> removeBook(@RequestParam("bookId") int bookId,@RequestHeader String token){
+		return bookservice.removeBook(bookId,token);
+	}
+	
+	@PostMapping("/upload")
+	public ResponseEntity<BookResponse> uploadImage(@RequestParam("imageFile") MultipartFile file,@RequestParam("bookId") int bookId,@RequestHeader String token) throws IOException {
+		return bookservice.uploadBookImage(token, file.getBytes(), bookId);
 	}
 	
 	@GetMapping("/getAllBooks")

248b342e85a9e33f5fffe872162d8f4f707c52af
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-22 03:48:03 +0530

    [Rupesh Patil].Updated BookStoreApllication Main  Class

--- BookStore/src/main/java/com/bookstore/BookStoreApplication.java
+++ BookStore/src/main/java/com/bookstore/BookStoreApplication.java
@@ -1,13 +1,19 @@
 package com.bookstore;
 
+import javax.annotation.Resource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-public class BookStoreApplication {
+import com.bookstore.service.IBookService;
 
+@SpringBootApplication
+public class BookStoreApplication{
+
+	@Resource
+	IBookService bookservice;
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
@@ -17,5 +23,5 @@ public class BookStoreApplication {
 	{
 		return new RestTemplate();
 	}
-
+	
 }

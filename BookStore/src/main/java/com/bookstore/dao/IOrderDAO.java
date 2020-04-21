6f2ff4ef8d42320761da4dc70dcc88c89cf0f4de
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-04-22 03:42:50 +0530

    [Rupesh Patil].updated OrderDao implementation

--- BookStore/src/main/java/com/bookstore/dao/IOrderDAO.java
+++ BookStore/src/main/java/com/bookstore/dao/IOrderDAO.java
@@ -1,8 +1,13 @@
 package com.bookstore.dao;
 
+import java.util.List;
+
 import com.bookstore.model.Order;
 
 public interface IOrderDAO {
 	public int addOrder(Order order);
+
 	public int deleteOrder(int bookId);
+
+	public List<Order> getOrderList(int userId);
 }

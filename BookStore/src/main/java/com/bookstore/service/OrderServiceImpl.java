96893ad6709c53ea4fb8dd2c3183220921594794
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-05-02 15:10:09 +0530

    [Rupesh Patil].Updated Order Service with confirm order and send order summery mail to costomer

--- BookStore/src/main/java/com/bookstore/service/OrderServiceImpl.java
+++ BookStore/src/main/java/com/bookstore/service/OrderServiceImpl.java
@@ -1,12 +1,22 @@
 package com.bookstore.service;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.client.RestTemplate;
 
 import com.auth0.jwt.exceptions.AlgorithmMismatchException;
@@ -14,6 +24,7 @@ import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.bookstore.dao.BookDaoImpl;
 import com.bookstore.dao.IOrderDAO;
+import com.bookstore.dto.MailResponse;
 import com.bookstore.exception.InvalidTokenOrExpiredException;
 import com.bookstore.exception.UserDoesNotExistException;
 import com.bookstore.model.Book;
@@ -23,6 +34,9 @@ import com.bookstore.response.OrderListResponse;
 import com.bookstore.response.OrderResponse;
 import com.bookstore.util.JwtTokenUtil;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -38,6 +52,11 @@ public class OrderServiceImpl implements IOrderservice {
 	@Autowired
 	private JwtTokenUtil generateToken;
 	static UserData userData;
+	 double finalAmount=0;
+	@Autowired
+	private JavaMailSender sender;
+	@Autowired
+	Configuration config;
 
 	/**
 	 *
@@ -56,7 +75,7 @@ public class OrderServiceImpl implements IOrderservice {
 			order.setEmail(userData.getEmail());
 			order.setPhNo(userData.getPhNo());
 			order.setTotal(order.getPrice() * order.getQuantity());
-
+			order.setBookImage(book.getBookImage());
 			if (orderDao.addOrder(order) > 0) {
 				System.out.println("Added successfully");
 				return ResponseEntity.status(HttpStatus.ACCEPTED).body(new OrderResponse(202, "Order Added to cart"));
@@ -70,19 +89,79 @@ public class OrderServiceImpl implements IOrderservice {
 	@Override
 	public ResponseEntity<Object> getCartList(String token) {
 		if (verifyUser(token)) {
-			Optional<List<Order>> orders=Optional.ofNullable(orderDao.getOrderList(userData.getUId()));
-			if(orders.isPresent()) {
-				return ResponseEntity.status(HttpStatus.ACCEPTED).body(new OrderListResponse(202, "total books in cart"+orders.get().size(), orders.get()));
-			}else {
-				return ResponseEntity.status(HttpStatus.ACCEPTED).body(new OrderResponse(202, "No any Books Added to cart"));
+			Optional<List<Order>> orders = Optional.ofNullable(orderDao.getOrderList(userData.getUId()));
+			if (orders.isPresent()) {
+				return ResponseEntity.status(HttpStatus.ACCEPTED)
+						.body(new OrderListResponse(202, "total books in cart" + orders.get().size(), orders.get()));
+			} else {
+				return ResponseEntity.status(HttpStatus.ACCEPTED)
+						.body(new OrderResponse(202, "No any Books Added to cart"));
 			}
 		}
 		throw new UserDoesNotExistException("User Does Not Exist", HttpStatus.BAD_REQUEST);
 	}
 
 	@Override
-	public ResponseEntity<Object> cancelOrder() {
+	public ResponseEntity<Object> updateQuantity(String token, Order order) {
+		if (verifyUser(token)) {
+			if (orderDao.updateQuantity(order) > 0) {
+				return ResponseEntity.status(HttpStatus.ACCEPTED).body(new OrderResponse(202, "Quantity Updatd"));
+			} else {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+						.body(new OrderResponse(500, "Error in Updated Quantity"));
+			}
+		} else {
+			throw new UserDoesNotExistException("User Does Not Exist", HttpStatus.BAD_REQUEST);
+		}
+	}
 
+	@Override
+	public ResponseEntity<Object> cancelOrder(String token, int bookId) {
+		if (verifyUser(token)) {
+			if (orderDao.deleteOrder(bookId) > 0) {
+				return ResponseEntity.status(HttpStatus.ACCEPTED)
+						.body(new OrderResponse(202, "Order Deleted SuccessFully"));
+			} else {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+						.body(new OrderResponse(500, "Error in Delete Order"));
+			}
+		} else {
+			throw new UserDoesNotExistException("User Does Not Exist", HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@Override
+	public ResponseEntity<Object> confirmOrder(String token, List<Order> order) {
+		if (verifyUser(token)) {
+			MimeMessage message = sender.createMimeMessage();
+			Map<String, Object> model = new HashMap<String, Object>();
+			order.forEach(s->{
+				double temp=0;
+				temp =s.getTotal();
+				finalAmount +=temp;
+			});
+			
+			model.put("name",userData.getFirstName());
+			model.put("total", finalAmount);
+			try {
+				MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_RELATED,
+						StandardCharsets.UTF_8.name());
+				Template template = config.getTemplate("order-summery.ftl");
+				String html = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+				helper.setTo(userData.getEmail());
+				helper.setText(html, true);
+				helper.setSubject("BookStore Order Summery");
+				helper.setFrom("pati.rupesh990@gmail.com");
+				sender.send(message);
+				orderDao.removeAllOrder(userData.getUId());
+				return ResponseEntity.status(HttpStatus.ACCEPTED).body(new MailResponse("Mail Sent", "202"));
+			} catch (MessagingException | IOException | TemplateException e) {
+				System.out.println("Error in message sending");
+				e.printStackTrace();
+			}
+		} else {
+			throw new UserDoesNotExistException("User Does Not Exist", HttpStatus.BAD_REQUEST);
+		}
 		return null;
 	}
 
@@ -99,5 +178,4 @@ public class OrderServiceImpl implements IOrderservice {
 			throw new InvalidTokenOrExpiredException("Invalid Token or Token Expired", HttpStatus.BAD_REQUEST);
 		}
 	}
-
 }
